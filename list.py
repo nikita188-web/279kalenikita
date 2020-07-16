b9d0078747ec8577f6b5fe1db8b5d1d0e57e35cb
nikita188-web <nikitakale188@gmail.com>
2020-07-16 14:48:12 +0530

    added

---
+++ list.py
@@ -0,0 +1,16 @@
+#operations on list
+
+list=[1,2,4,3,5,2]    #list create
+print(list)
+list.insert(1,10)
+print(list)
+list.remove(2)
+print(list)
+list.append(11)
+print(list)
+list.sort()
+print(list)
+list.pop()
+print(list)
+list.reverse()
+print(list)
