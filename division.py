ede03a9dd6ea6175bdb24e08429bef4ef9874188
nikita188-web <nikitakale188@gmail.com>
2020-07-16 15:29:26 +0530

    added

--- division.py
+++ division.py
@@ -1,11 +1,25 @@
-<<<<<<< HEAD
-#assignment no 1-c
-=======
-#assignment no 1-D(1)
->>>>>>> origin/master
+
+
+
+#assignment no 1-D(A)
 
 num1=int(input())       #read  2 integer input
 num2=int(input())
 
 print(num1//num2)    #for integer division
 print(num1/num2)     #for flot division
+
+#assignment no 1-D(B)
+list=[]
+list.insert(0,5)
+list.insert(1,10)
+list.insert(0,6)
+print(list)
+list.remove(6)
+list.append(9)
+list.append(1)
+list.sort()
+print(list)
+list.pop()
+list.reverse()
+print(list)
